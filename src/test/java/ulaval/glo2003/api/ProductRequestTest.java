1a6f9f05b130742fc9899eecbb98206c8c3df00a
Jérôme <jerome.levesque.14@ulaval.ca>
2023-02-14 14:59:09 -0500

    test : testing of ProductRequest class

---
+++ src/test/java/ulaval/glo2003/api/ProductRequestTest.java
@@ -0,0 +1,69 @@
+package ulaval.glo2003.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ulaval.glo2003.api.exceptionHandling.MissingParamException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ProductRequestTest {
+
+    private ProductRequest productRequest;
+    private String nonNullTitle;
+    private String nonNullDescription;
+    private Double nonNullSuggestedPrice;
+    private String nonNullCategory;
+
+    @BeforeEach
+    public void prepareNonNullParameters() {
+        nonNullTitle = "Iphone XR";
+        nonNullDescription = "A relatively new Iphone working as good as a new one";
+        nonNullSuggestedPrice = 200d;
+        nonNullCategory = "electronics";
+    }
+
+    @BeforeEach
+    public void preparevalidProductRequest() {
+        productRequest = new ProductRequest();
+        productRequest.title = nonNullTitle;
+        productRequest.description = nonNullDescription;
+        productRequest.suggestedPrice = nonNullSuggestedPrice;
+        productRequest.category = nonNullCategory;
+    }
+
+    @Test
+    public void validateMethodThrowsWhenNullTitle() {
+        productRequest.title = null;
+
+        assertThrows(
+                MissingParamException.class,
+                () -> productRequest.validateProductNonNullParameter());
+    }
+
+    @Test
+    public void validateMethodThrowsWhenNullDescription() {
+        productRequest.description = null;
+
+        assertThrows(
+                MissingParamException.class,
+                () -> productRequest.validateProductNonNullParameter());
+    }
+
+    @Test
+    public void validateMethodThrowsWhenNullSuggestedPrice() {
+        productRequest.suggestedPrice = null;
+
+        assertThrows(
+                MissingParamException.class,
+                () -> productRequest.validateProductNonNullParameter());
+    }
+
+    @Test
+    public void validateMethodThrowsWhenNullCategory() {
+        productRequest.category = null;
+
+        assertThrows(
+                MissingParamException.class,
+                () -> productRequest.validateProductNonNullParameter());
+    }
+}
