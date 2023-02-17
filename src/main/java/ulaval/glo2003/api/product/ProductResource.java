d1e21c7b6234c45654c933592c4caee6f8c3044f
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2023-02-17 10:29:51 -0500

    feat: creation of offer (#42)
    * feat: creation of offer
    
    * style: typo
    
    * fix: validateParameters before searching seller
    
    * style: spotless format

--- src/main/java/ulaval/glo2003/api/product/ProductResource.java
+++ src/main/java/ulaval/glo2003/api/product/ProductResource.java
@@ -6,6 +6,8 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 import java.util.List;
+import ulaval.glo2003.api.offer.OfferRequest;
+import ulaval.glo2003.domain.Offer;
 import ulaval.glo2003.domain.Product;
 import ulaval.glo2003.domain.Seller;
 
@@ -21,12 +23,13 @@ public class ProductResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createProducts(
             @Context UriInfo uriInfo, @HeaderParam("X-Seller-Id") String xSellerId, ProductRequest productRequest) {
+        productRequest.validateProductNonNullParameter();
+
         Seller foundSeller = sellers.stream()
                 .filter(seller -> xSellerId.equals(seller.getId()))
                 .findFirst()
                 .orElseThrow(() -> new NotFoundException(String.format("Seller with id '%s' not found", xSellerId)));
 
-        productRequest.validateProductNonNullParameter();
         Product product = new Product(
                 productRequest.title,
                 productRequest.description,
@@ -51,5 +54,25 @@ public class ProductResource {
         return Response.ok()
                 .entity(new ProductResponse(foundSeller, foundProduct))
                 .build();
+    }
+
+    @POST
+    @Path("/{productId}/offers")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createOffer(
+            @HeaderParam("X-Buyer-Username") String xBuyerUsername,
+            @PathParam("productId") String productId,
+            OfferRequest offerRequest) {
+        offerRequest.validateOfferNonNullParameter();
+
+        Seller foundSeller = sellers.stream()
+                .filter(seller -> seller.getProductById(productId) != null)
+                .findFirst()
+                .orElseThrow(() -> new NotFoundException(String.format("Product with id '%s' not found", productId)));
+
+        Offer offer = new Offer(xBuyerUsername, offerRequest.amount, offerRequest.message);
+        foundSeller.getProductById(productId).addOffer(offer);
+
+        return Response.status(Response.Status.CREATED).build();
     }
 }
--- src/main/java/ulaval/glo2003/domain/Offer.java
+++ src/main/java/ulaval/glo2003/domain/Offer.java
@@ -12,7 +12,7 @@ public class Offer {
 
     public Offer(String username, Double amount, String message) {
         this.username = username;
-        this.amount = amount;
+        this.amount = Math.round(amount * 100d) / 100d;
         this.message = message;
 
         validateOfferParameters();
