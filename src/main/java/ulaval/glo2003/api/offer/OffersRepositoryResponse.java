22ae8daad5db99befe2f9cc8f3ffc9fbd6c420fd
Jérôme <jerome.levesque.14@ulaval.ca>
2023-02-18 15:02:09 -0500

    refactor : refactor OffersResponse to OffersRepositoryResponse

---
+++ src/main/java/ulaval/glo2003/api/offer/OffersRepositoryResponse.java
@@ -0,0 +1,9 @@
+package ulaval.glo2003.api.offer;
+
+public class OffersRepositoryResponse {
+
+    public Integer count;
+    public Double avgAmount;
+
+    public OffersRepositoryResponse() {}
+}
