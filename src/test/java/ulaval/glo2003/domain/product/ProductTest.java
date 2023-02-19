bf6be3a9965ee3d0797770d8bbbe67ebbe959bfe
Jeremi Girard <jeremigirard@hotmail.com>
2023-02-19 15:46:43 -0500

    Refactor : Ajusted `ProductTest` and `SellerTest`

--- src/test/java/ulaval/glo2003/domain/product/ProductTest.java
+++ src/test/java/ulaval/glo2003/domain/product/ProductTest.java
@@ -1,90 +1,39 @@
 package ulaval.glo2003.domain.product;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ulaval.glo2003.domain.exceptions.InvalidParamException;
-import ulaval.glo2003.domain.exceptions.NotPermittedException;
+import org.mockito.Mock;
 import ulaval.glo2003.domain.offer.Offer;
 
 public class ProductTest {
 
     private Product product;
-    private final String validTitle = "Iphone XR";
-    private final String validDescription = "A relatively new Iphone working as good as a new one";
-    private final Double validSuggestedPrice = 200d;
-    private final String validCategory = "electronics";
 
-    private final Offer validOffer = new Offer(
-            "User0",
-            200d,
-            "Lorem ipsum dolor sit amet, consectetur"
-                    + " adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+    @Mock
+    Offer offerMock = mock(Offer.class);
 
     @BeforeEach
     public void prepareProduct() {
+        String validTitle = "Iphone XR";
+        String validDescription = "A relatively new Iphone working as good as a new one";
+        Double validSuggestedPrice = 200d;
+        String validCategory = "electronics";
         product = new Product(validTitle, validDescription, validSuggestedPrice, validCategory);
     }
 
     @Test
-    public void constructorThrowsWhenEmptyTitle() {
-        String emptyTitle = " \n\t";
-
-        assertThrows(
-                InvalidParamException.class,
-                () -> new Product(emptyTitle, validDescription, validSuggestedPrice, validCategory));
-    }
-
-    @Test
-    public void constructorThrowsWhenEmptyDescription() {
-        String emptyDescription = " \n\t";
-
-        assertThrows(
-                InvalidParamException.class,
-                () -> new Product(validTitle, emptyDescription, validSuggestedPrice, validCategory));
-    }
-
-    @Test
-    public void constructorThrowsWhenSuggestedPriceUnder1() {
-        Double under1SuggestedPrice = 0.5d;
-
-        assertThrows(
-                InvalidParamException.class,
-                () -> new Product(validTitle, validDescription, under1SuggestedPrice, validCategory));
-    }
-
-    @Test
-    public void constructorThrowsWhenCategoryDoesntExist() {
-        String inexistentCategory = "Apple products";
-
-        assertThrows(
-                InvalidParamException.class,
-                () -> new Product(validTitle, validDescription, validSuggestedPrice, inexistentCategory));
-    }
-
-    @Test
     public void canAddValidOffer() {
-        product.addOffer(validOffer);
+        doReturn("usertest").when(offerMock).getUsername();
+        doReturn(200d).when(offerMock).getAmount();
+
+        product.addOffer(offerMock);
 
         Optional<Offer> offer = Optional.ofNullable(product.getOffers().get(0));
         assertThat(offer.isPresent()).isTrue();
-    }
-
-    @Test
-    public void addOfferMethodThrowsWhenOfferAmountLowerSuggestedPrice() {
-        Offer invalidOffer = new Offer(validOffer.getUsername(), 150d, validOffer.getMessage());
-
-        assertThrows(InvalidParamException.class, () -> product.addOffer(invalidOffer));
-    }
-
-    @Test
-    public void addOfferMethodThrowsWhenBuyerAlreadyMadeOffer() {
-        product.addOffer(validOffer);
-        Offer otherOfferSameUser = new Offer(validOffer.getUsername(), validOffer.getAmount(), validOffer.getMessage());
-
-        assertThrows(NotPermittedException.class, () -> product.addOffer(otherOfferSameUser));
     }
 }
--- src/test/java/ulaval/glo2003/domain/seller/SellerTest.java
+++ src/test/java/ulaval/glo2003/domain/seller/SellerTest.java
@@ -1,215 +1,48 @@
 package ulaval.glo2003.domain.seller;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Spy;
-import ulaval.glo2003.domain.exceptions.InvalidParamException;
+import org.mockito.Mock;
 import ulaval.glo2003.domain.product.Product;
 
 public class SellerTest {
 
     private Seller seller;
-    private final String validName = "Bob";
-    private final String validBirthdate = "2000-01-01";
-    private final String validPhoneNumber = "14181234567";
-    private final String validEmail = "bob@gmail.com";
-    private final String validBio = "My name is Bob!";
-    private final Product validProduct = new Product("Title", "Description", 200.0, "electronics");
 
-    @Spy
-    Seller sellerSpy;
+    @Mock
+    Product productMock = mock(Product.class);
 
     @BeforeEach
     public void initSeller() {
+        String validName = "Bob";
+        String validBirthdate = "2000-01-01";
+        String validPhoneNumber = "14181234567";
+        String validEmail = "bob@gmail.com";
+        String validBio = "My name is Bob!";
+
         seller = new Seller(validName, validBirthdate, validEmail, validPhoneNumber, validBio);
-        sellerSpy = spy(seller);
-    }
 
-    @Test
-    public void constructorThrowsWhenNameInvalid() {
-        InvalidParamException invalidParamException = new InvalidParamException("name");
-        String invalidName = " \n \r \t";
-
-        InvalidParamException thrownInvalidName = assertThrows(
-                InvalidParamException.class,
-                () -> new Seller(invalidName, validBirthdate, validEmail, validPhoneNumber, validBio),
-                "Expected new Seller() to throw");
-
-        assertThat(thrownInvalidName.errorDescription.description)
-                .isEqualTo(invalidParamException.errorDescription.description);
-    }
-
-    @Test
-    public void constructorThrowsWhenBirthdateInvalidFormat() {
-        InvalidParamException invalidParamException = new InvalidParamException("birthdate");
-        String invalidFormat = "2001/12/20";
-
-        InvalidParamException thrownInvalidFormat = assertThrows(
-                InvalidParamException.class,
-                () -> new Seller(validName, invalidFormat, validEmail, validPhoneNumber, validBio),
-                "Expected new Seller() to throw");
-        assertThat(thrownInvalidFormat.errorDescription.description)
-                .isEqualTo(invalidParamException.errorDescription.description);
-    }
-
-    @Test
-    public void constructorThrowsWhenBirthdateInvalidDate() {
-        InvalidParamException invalidParamException = new InvalidParamException("birthdate");
-        String invalidDate = "2001-13-20";
-
-        InvalidParamException thrownInvalidDate = assertThrows(
-                InvalidParamException.class,
-                () -> new Seller(validName, invalidDate, validEmail, validPhoneNumber, validBio),
-                "Expected new Seller() to throw");
-        assertThat(thrownInvalidDate.errorDescription.description)
-                .isEqualTo(invalidParamException.errorDescription.description);
-    }
-
-    @Test
-    void functionIs18orMoreCheckWithOffset() {
-        OffsetDateTime fixedDateTime = OffsetDateTime.of(2018, 1, 2, 2, 0, 0, 0, ZoneOffset.ofHours(3));
-        String invalidBirthDate1 = "2000-01-03";
-        String invalidBirthDate2 = "2000-01-02";
-        String validDate1 = "2000-01-01";
-
-        when(sellerSpy.currentTime()).thenReturn(fixedDateTime);
-
-        assertFalse(sellerSpy.is18orMore(invalidBirthDate1));
-        assertFalse(sellerSpy.is18orMore(invalidBirthDate2));
-        assertTrue(sellerSpy.is18orMore(validDate1));
-    }
-
-    @Test
-    public void constructorThrowsWhenEmailMissingIdentifier() {
-        InvalidParamException invalidParamException = new InvalidParamException("email");
-        String invalidEmail = "@Bob.com";
-
-        InvalidParamException thrownInvalidEmail = assertThrows(
-                InvalidParamException.class,
-                () -> new Seller(validName, validBirthdate, invalidEmail, validPhoneNumber, validBio),
-                "Expected new Seller() to throw");
-        assertThat(thrownInvalidEmail.errorDescription.description)
-                .isEqualTo(invalidParamException.errorDescription.description);
-    }
-
-    @Test
-    public void constructorThrowsWhenEmailMissingAt() {
-        InvalidParamException invalidParamException = new InvalidParamException("email");
-        String invalidEmail = "Bob.com";
-
-        InvalidParamException thrownInvalidEmail = assertThrows(
-                InvalidParamException.class,
-                () -> new Seller(validName, validBirthdate, invalidEmail, validPhoneNumber, validBio),
-                "Expected new Seller() to throw");
-        assertThat(thrownInvalidEmail.errorDescription.description)
-                .isEqualTo(invalidParamException.errorDescription.description);
-    }
-
-    @Test
-    public void constructorThrowsWhenEmailMissingService() {
-        InvalidParamException invalidParamException = new InvalidParamException("email");
-        String invalidEmail = "Bob@.com";
-
-        InvalidParamException thrownInvalidEmail = assertThrows(
-                InvalidParamException.class,
-                () -> new Seller(validName, validBirthdate, invalidEmail, validPhoneNumber, validBio),
-                "Expected new Seller() to throw");
-        assertThat(thrownInvalidEmail.errorDescription.description)
-                .isEqualTo(invalidParamException.errorDescription.description);
-    }
-
-    @Test
-    public void constructorThrowsWhenEmailMissingDot() {
-        InvalidParamException invalidParamException = new InvalidParamException("email");
-        String invalidEmail = "Bob@gmailcom";
-
-        InvalidParamException thrownInvalidEmail = assertThrows(
-                InvalidParamException.class,
-                () -> new Seller(validName, validBirthdate, invalidEmail, validPhoneNumber, validBio),
-                "Expected new Seller() to throw");
-        assertThat(thrownInvalidEmail.errorDescription.description)
-                .isEqualTo(invalidParamException.errorDescription.description);
-    }
-
-    @Test
-    public void constructorThrowsWhenEmailMissingExtension() {
-        InvalidParamException invalidParamException = new InvalidParamException("email");
-        String invalidEmail = "Bob@gmail.";
-
-        InvalidParamException thrownInvalidEmail = assertThrows(
-                InvalidParamException.class,
-                () -> new Seller(validName, validBirthdate, invalidEmail, validPhoneNumber, validBio),
-                "Expected new Seller() to throw");
-        assertThat(thrownInvalidEmail.errorDescription.description)
-                .isEqualTo(invalidParamException.errorDescription.description);
-    }
-
-    @Test
-    public void constructorThrowsWhenPhoneNumberIsTooLong() {
-        InvalidParamException invalidParamException = new InvalidParamException("phone number");
-        String invalidPhoneNumber = "111234567890";
-
-        InvalidParamException thrownInvalidPhoneNumber = assertThrows(
-                InvalidParamException.class,
-                () -> new Seller(validName, validBirthdate, validEmail, invalidPhoneNumber, validBio),
-                "Expected new Seller() to throw");
-        assertThat(thrownInvalidPhoneNumber.errorDescription.description)
-                .isEqualTo(invalidParamException.errorDescription.description);
-    }
-
-    @Test
-    public void constructorThrowsWhenPhoneNumberIsTooShort() {
-        InvalidParamException invalidParamException = new InvalidParamException("phone number");
-        String invalidPhoneNumber = "1234567890";
-
-        InvalidParamException thrownInvalidPhoneNumber = assertThrows(
-                InvalidParamException.class,
-                () -> new Seller(validName, validBirthdate, validEmail, invalidPhoneNumber, validBio),
-                "Expected new Seller() to throw");
-        assertThat(thrownInvalidPhoneNumber.errorDescription.description)
-                .isEqualTo(invalidParamException.errorDescription.description);
-    }
-
-    @Test
-    public void constructorThrowsWhenPhoneNumberContainOtherChar() {
-        InvalidParamException invalidParamException = new InvalidParamException("phone number");
-        String invalidPhoneNumber = "+1234567890";
-
-        InvalidParamException thrownInvalidPhoneNumber = assertThrows(
-                InvalidParamException.class,
-                () -> new Seller(validName, validBirthdate, validEmail, invalidPhoneNumber, validBio),
-                "Expected new Seller() to throw");
-        assertThat(thrownInvalidPhoneNumber.errorDescription.description)
-                .isEqualTo(invalidParamException.errorDescription.description);
-    }
-
-    @Test
-    public void constructorThrowsWhenBioInvalid() {
-        InvalidParamException invalidParamException = new InvalidParamException("bio");
-        String invalidBio = " \n \r \t";
-
-        InvalidParamException thrownInvalidBio = assertThrows(
-                InvalidParamException.class,
-                () -> new Seller(validName, validBirthdate, validEmail, validPhoneNumber, invalidBio),
-                "Expected new Seller() to throw");
-
-        assertThat(thrownInvalidBio.errorDescription.description)
-                .isEqualTo(invalidParamException.errorDescription.description);
+        doReturn("id-test").when(productMock).getId();
     }
 
     @Test
     public void canAddProduct() {
-        seller.addProduct(validProduct);
-        Optional<Product> product = Optional.ofNullable(seller.getProductById(validProduct.getId()));
+        seller.addProduct(productMock);
+        Optional<Product> product = Optional.ofNullable(seller.getProductById(productMock.getId()));
 
         assertThat(seller.getProducts().size()).isEqualTo(1);
         assertThat(product.isPresent()).isTrue();
+    }
+
+    @Test
+    public void doesNotAddSameProductTwice() {
+        seller.addProduct(productMock);
+        seller.addProduct(productMock);
+
+        assertThat(seller.getProducts().size()).isEqualTo(1);
     }
 }
