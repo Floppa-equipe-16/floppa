3adacab4de9ca3d87bd6552b57bf47ffba72a72d
Jeremi Girard <jeremigirard@hotmail.com>
2023-02-19 15:45:45 -0500

    Feat : Added `ProductParamValidator`

--- src/main/java/ulaval/glo2003/domain/product/Product.java
+++ src/main/java/ulaval/glo2003/domain/product/Product.java
@@ -3,8 +3,6 @@ package ulaval.glo2003.domain.product;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.UUID;
-import ulaval.glo2003.domain.exceptions.InvalidParamException;
-import ulaval.glo2003.domain.exceptions.NotPermittedException;
 import ulaval.glo2003.domain.offer.Offer;
 
 public class Product {
@@ -14,8 +12,8 @@ public class Product {
     private final String category;
     private final String id;
     private final String createdAt;
-
     private final ArrayList<Offer> offers;
+    private final ProductParamValidator productParamValidator;
 
     public Product(String title, String description, Double suggestedPrice, String category) {
         this.title = title;
@@ -24,17 +22,11 @@ public class Product {
         this.category = category;
         this.offers = new ArrayList<>();
 
-        validateProductParameters();
+        productParamValidator = new ProductParamValidator(this);
+        productParamValidator.validateProductParamThrowIfInvalid();
 
         id = UUID.randomUUID().toString();
         createdAt = Instant.now().toString();
-    }
-
-    private void validateProductParameters() {
-        if (title.isBlank()) throw new InvalidParamException("title");
-        if (description.isBlank()) throw new InvalidParamException("description");
-        if (!doesCategoryExist()) throw new InvalidParamException("category");
-        if (isSuggestedPriceUnder1()) throw new InvalidParamException("suggestedPrice");
     }
 
     public String getTitle() {
@@ -66,34 +58,7 @@ public class Product {
     }
 
     public void addOffer(Offer offer) {
-        validateOfferEligible(offer);
+        productParamValidator.validateOfferEligibleThrowIfInvalid(offer);
         offers.add(offer);
-    }
-
-    private boolean isSuggestedPriceUnder1() {
-        return suggestedPrice.intValue() < 1;
-    }
-
-    private boolean doesCategoryExist() {
-        try {
-            ProductCategory.valueOf(this.category);
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
-    }
-
-    private void validateOfferEligible(Offer offer) {
-        if (!isOfferAmountAtleastSuggestedPrice(offer.getAmount())) throw new InvalidParamException("amount");
-        if (hasBuyerAlreadyMadeAnOffer(offer.getUsername()))
-            throw new NotPermittedException("user with username " + offer.getUsername() + " has already made an offer");
-    }
-
-    private boolean hasBuyerAlreadyMadeAnOffer(String buyerUsername) {
-        return offers.stream().anyMatch(offer -> buyerUsername.equals(offer.getUsername()));
-    }
-
-    private boolean isOfferAmountAtleastSuggestedPrice(Double offerAmount) {
-        return offerAmount >= suggestedPrice;
     }
 }
---
+++ src/main/java/ulaval/glo2003/domain/product/ProductParamValidator.java
@@ -0,0 +1,56 @@
+package ulaval.glo2003.domain.product;
+
+import ulaval.glo2003.domain.exceptions.InvalidParamException;
+import ulaval.glo2003.domain.exceptions.NotPermittedException;
+import ulaval.glo2003.domain.offer.Offer;
+
+class ProductParamValidator {
+    private final Product product;
+
+    public ProductParamValidator(Product product) {
+        this.product = product;
+    }
+
+    public void validateProductParamThrowIfInvalid() {
+        if (isTitleInvalid(product.getTitle())) throw new InvalidParamException("title");
+        if (isDescriptionInvalid(product.getDescription())) throw new InvalidParamException("description");
+        if (isCategoryInvalid(product.getCategory())) throw new InvalidParamException("category");
+        if (isSuggestedPriceInvalid(product.getSuggestedPrice())) throw new InvalidParamException("suggested price");
+    }
+
+    public void validateOfferEligibleThrowIfInvalid(Offer offer) {
+        if (isOfferAmountLessThenSuggestedPrice(offer.getAmount())) throw new InvalidParamException("amount");
+        if (hasBuyerAlreadyMadeAnOffer(offer.getUsername()))
+            throw new NotPermittedException(
+                    "user with username `" + offer.getUsername() + "` has already made an offer");
+    }
+
+    protected boolean isTitleInvalid(String s) {
+        return s.isBlank();
+    }
+
+    protected boolean isDescriptionInvalid(String s) {
+        return s.isBlank();
+    }
+
+    protected boolean isSuggestedPriceInvalid(Double suggestedPrice) {
+        return suggestedPrice < 1d;
+    }
+
+    protected boolean isCategoryInvalid(String category) {
+        try {
+            ProductCategory.valueOf(category);
+        } catch (Exception e) {
+            return true;
+        }
+        return false;
+    }
+
+    protected boolean isOfferAmountLessThenSuggestedPrice(Double offerAmount) {
+        return offerAmount < product.getSuggestedPrice();
+    }
+
+    protected boolean hasBuyerAlreadyMadeAnOffer(String buyerUsername) {
+        return product.getOffers().stream().anyMatch(offer -> buyerUsername.equals(offer.getUsername()));
+    }
+}
