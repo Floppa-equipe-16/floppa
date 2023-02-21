fee9275b4da4f4c3e2f000ce1a360a229b14d6b6
Jeremi Girard <jeremigirard@hotmail.com>
2023-02-21 15:24:41 -0500

    Test : merge adjustment

---
+++ src/test/java/ulaval/glo2003/domain/product/ProductValidatorTest.java
@@ -0,0 +1,172 @@
+package ulaval.glo2003.domain.product;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import ulaval.glo2003.domain.exceptions.InvalidParamException;
+import ulaval.glo2003.domain.exceptions.NotPermittedException;
+import ulaval.glo2003.domain.offer.Offer;
+
+public class ProductValidatorTest {
+
+    private ProductValidator productValidator;
+
+    @Mock
+    private Product productMock = mock(Product.class);
+
+    @Mock
+    private Offer offerMock = mock(Offer.class);
+
+    @Spy
+    private ProductValidator productValidatorSpy;
+
+    @BeforeEach
+    void ProductParamValidatorTest() {
+        productValidator = new ProductValidator(productMock);
+        productValidatorSpy = spy(productValidator);
+
+        setAllValidatorInSpyToValid();
+    }
+
+    private void setAllValidatorInSpyToValid(){
+        doReturn(false).when(productValidatorSpy).isSellerIdInvalid(Mockito.any());
+        doReturn(false).when(productValidatorSpy).isTitleInvalid(Mockito.any());
+        doReturn(false).when(productValidatorSpy).isDescriptionInvalid(Mockito.any());
+        doReturn(false).when(productValidatorSpy).isCategoryInvalid(Mockito.any());
+        doReturn(false).when(productValidatorSpy).isSuggestedPriceInvalid(Mockito.any());
+        doReturn(false).when(productValidatorSpy).isOfferAmountLessThenSuggestedPrice(Mockito.any());
+        doReturn(false).when(productValidatorSpy).hasBuyerAlreadyMadeAnOffer(Mockito.any());
+    }
+
+    @Test
+    void testInvalidIdInValidateParamThrowIfInvalid() {
+        doReturn(true).when(productValidatorSpy).isSellerIdInvalid(Mockito.any());
+
+        InvalidParamException thrownInvalidId = assertThrows(
+                InvalidParamException.class, () -> productValidatorSpy.validateParamThrowIfInvalid());
+        assertThat(thrownInvalidId.errorDescription.description).isEqualTo("Invalid parameter 'id'.");
+    }
+    @Test
+    void testInvalidTitleInValidateParamThrowIfInvalid() {
+        doReturn(true).when(productValidatorSpy).isTitleInvalid(Mockito.any());
+
+        InvalidParamException thrownInvalidTitle = assertThrows(
+                InvalidParamException.class, () -> productValidatorSpy.validateParamThrowIfInvalid());
+        assertThat(thrownInvalidTitle.errorDescription.description).isEqualTo("Invalid parameter 'title'.");
+    }
+
+    @Test
+    void testInvalidDescriptionInValidateParamThrowIfInvalid() {
+        doReturn(true).when(productValidatorSpy).isDescriptionInvalid(Mockito.any());
+
+        InvalidParamException thrownInvalidDescription = assertThrows(
+                InvalidParamException.class, () -> productValidatorSpy.validateParamThrowIfInvalid());
+        assertThat(thrownInvalidDescription.errorDescription.description).isEqualTo("Invalid parameter 'description'.");
+    }
+
+    @Test
+    void testInvalidCategoryInValidateParamThrowIfInvalid() {
+        doReturn(true).when(productValidatorSpy).isCategoryInvalid(Mockito.any());
+
+        InvalidParamException thrownInvalidCategory = assertThrows(
+                InvalidParamException.class, () -> productValidatorSpy.validateParamThrowIfInvalid());
+        assertThat(thrownInvalidCategory.errorDescription.description).isEqualTo("Invalid parameter 'category'.");
+    }
+
+    @Test
+    void testInvalidSuggestedPriceInValidateParamThrowIfInvalid() {
+        doReturn(true).when(productValidatorSpy).isSuggestedPriceInvalid(Mockito.any());
+
+        InvalidParamException thrownInvalidSuggestedPrice = assertThrows(
+                InvalidParamException.class, () -> productValidatorSpy.validateParamThrowIfInvalid());
+        assertThat(thrownInvalidSuggestedPrice.errorDescription.description)
+                .isEqualTo("Invalid parameter 'suggested price'.");
+    }
+
+    @Test
+    void testInvalidAmountInvalidateOfferEligibleThrowIfInvalid() {
+        doReturn(true).when(productValidatorSpy).isOfferAmountLessThenSuggestedPrice(Mockito.any());
+
+        InvalidParamException thrownInvalidAmount = assertThrows(
+                InvalidParamException.class,
+                () -> productValidatorSpy.validateOfferEligibleThrowIfInvalid(offerMock));
+        assertThat(thrownInvalidAmount.errorDescription.description).isEqualTo("Invalid parameter 'amount'.");
+    }
+
+    @Test
+    void testInvalidUsernameInvalidateOfferEligibleThrowIfInvalid() {
+        String username = "Username Test";
+        doReturn(username).when(offerMock).getUsername();
+        doReturn(true).when(productValidatorSpy).hasBuyerAlreadyMadeAnOffer(Mockito.any());
+
+        NotPermittedException thrownInvalidUsername = assertThrows(
+                NotPermittedException.class,
+                () -> productValidatorSpy.validateOfferEligibleThrowIfInvalid(offerMock));
+        assertThat(thrownInvalidUsername.errorDescription.description)
+                .isEqualTo("Not permitted action 'user with username `" + username + "` has already made an offer'.");
+    }
+
+    @Test
+    void testIsIdInvalid() {
+        String invalidId = " \n \r \t";
+
+        assertTrue(productValidator.isSellerIdInvalid(invalidId));
+    }
+    @Test
+    void testIsTitleInvalid() {
+        String invalidTitle = " \n \r \t";
+
+        assertTrue(productValidator.isTitleInvalid(invalidTitle));
+    }
+
+    @Test
+    void testIsDescriptionInvalid() {
+        String invalidDescription = " \n \r \t";
+
+        assertTrue(productValidator.isDescriptionInvalid(invalidDescription));
+    }
+
+    @Test
+    void testIsCategoryInvalid() {
+        String invalidCategory = "InvalidCategory";
+
+        assertTrue(productValidator.isCategoryInvalid(invalidCategory));
+    }
+
+    @Test
+    void testIsSuggestedPriceInvalid() {
+        Double suggestedPrice = 0.5;
+
+        assertTrue(productValidator.isSuggestedPriceInvalid(suggestedPrice));
+    }
+
+    @Test
+    void testIsOfferAmountLessThenSuggestedPrice() {
+        doReturn(20d).when(productMock).getSuggestedPrice();
+        Double offerAmount = 19.9d;
+
+        assertTrue(productValidator.isOfferAmountLessThenSuggestedPrice(offerAmount));
+    }
+
+    @Test
+    void testHasBuyerAlreadyMadeAnOffer() {
+        ArrayList<Offer> offers = new ArrayList<>();
+        offers.add(offerMock);
+        String username = "Username Test";
+        String notAddedUsername = "test";
+
+        doReturn(username).when(offerMock).getUsername();
+        doReturn(offers).when(productMock).getOffers();
+
+        assertTrue(productValidator.hasBuyerAlreadyMadeAnOffer(username));
+        assertFalse(productValidator.hasBuyerAlreadyMadeAnOffer(notAddedUsername));
+    }
+}
---
+++ src/test/java/ulaval/glo2003/domain/seller/SellerValidatorTest.java
@@ -0,0 +1,191 @@
+package ulaval.glo2003.domain.seller;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import ulaval.glo2003.domain.exceptions.InvalidParamException;
+
+public class SellerValidatorTest {
+
+    private SellerValidator sellerValidator;
+
+    @Mock
+    Seller sellerMock = mock(Seller.class);
+
+    @Spy
+    SellerValidator sellerValidatorSpy;
+
+    @BeforeEach
+    void SellerParamValidatorTest() {
+        sellerValidator = new SellerValidator(sellerMock);
+        sellerValidatorSpy = spy(sellerValidator);
+
+        setAllValidatorInSpyToValid();
+    }
+
+    private void setAllValidatorInSpyToValid(){
+        doReturn(false).when(sellerValidatorSpy).isNameInvalid(Mockito.any());
+        doReturn(false).when(sellerValidatorSpy).isBirthdateInvalid(Mockito.any());
+        doReturn(false).when(sellerValidatorSpy).isPhoneNumberInvalid(Mockito.any());
+        doReturn(false).when(sellerValidatorSpy).isEmailInvalid(Mockito.any());
+        doReturn(false).when(sellerValidatorSpy).isBioInvalid(Mockito.any());
+    }
+
+    @Test
+    void testInvalidNameInValidateSellerParamThrowIfInvalid() {
+        doReturn(true).when(sellerValidatorSpy).isNameInvalid(Mockito.any());
+
+        InvalidParamException thrownInvalidName = assertThrows(
+                InvalidParamException.class, () -> sellerValidatorSpy.validateParamThrowIfInvalid());
+        assertThat(thrownInvalidName.errorDescription.description).isEqualTo("Invalid parameter 'name'.");
+    }
+
+    @Test
+    void testInvalidBirthdateInValidateSellerParamThrowIfInvalid() {
+        doReturn(true).when(sellerValidatorSpy).isBirthdateInvalid(Mockito.any());
+
+        InvalidParamException thrownInvalidName = assertThrows(
+                InvalidParamException.class, () -> sellerValidatorSpy.validateParamThrowIfInvalid());
+        assertThat(thrownInvalidName.errorDescription.description).isEqualTo("Invalid parameter 'birthdate'.");
+    }
+
+    @Test
+    void testInvalidEmailInValidateSellerParamThrowIfInvalid() {
+        doReturn(true).when(sellerValidatorSpy).isEmailInvalid(Mockito.any());
+
+        InvalidParamException thrownInvalidName = assertThrows(
+                InvalidParamException.class, () -> sellerValidatorSpy.validateParamThrowIfInvalid());
+        assertThat(thrownInvalidName.errorDescription.description).isEqualTo("Invalid parameter 'email'.");
+    }
+
+    @Test
+    void testInvalidPhoneNumberInValidateSellerParamThrowIfInvalid() {
+        doReturn(true).when(sellerValidatorSpy).isPhoneNumberInvalid(Mockito.any());
+
+        InvalidParamException thrownInvalidName = assertThrows(
+                InvalidParamException.class, () -> sellerValidatorSpy.validateParamThrowIfInvalid());
+        assertThat(thrownInvalidName.errorDescription.description).isEqualTo("Invalid parameter 'phone number'.");
+    }
+
+    @Test
+    void testInvalidBioInValidateSellerParamThrowIfInvalid() {
+        doReturn(true).when(sellerValidatorSpy).isBioInvalid(Mockito.any());
+
+        InvalidParamException thrownInvalidName = assertThrows(
+                InvalidParamException.class, () -> sellerValidatorSpy.validateParamThrowIfInvalid());
+        assertThat(thrownInvalidName.errorDescription.description).isEqualTo("Invalid parameter 'bio'.");
+    }
+
+    @Test
+    void testisNameInvalid() {
+        String emptyName = " \r \t \n";
+
+        assertTrue(sellerValidator.isNameInvalid(emptyName));
+    }
+
+    @Test
+    void testisBioInvalid() {
+        String emptyBio = " \r \t \n";
+
+        assertTrue(sellerValidator.isBioInvalid(emptyBio));
+    }
+
+    @Test
+    void testIsPhoneNumberTooLong() {
+        String phoneNumber = "1234567890123";
+
+        assertTrue(sellerValidator.isPhoneNumberInvalid(phoneNumber));
+    }
+
+    @Test
+    void testIsPhoneNumberTooShort() {
+        String phoneNumber = "123456789";
+
+        assertTrue(sellerValidator.isPhoneNumberInvalid(phoneNumber));
+    }
+
+    @Test
+    void testIsPhoneNumberContainOtherChar() {
+        String phoneNumber = "+1234567890";
+
+        assertTrue(sellerValidator.isPhoneNumberInvalid(phoneNumber));
+    }
+
+    @Test
+    void testIsPhoneNumberValid() {
+        String phoneNumber = "11234567890";
+
+        assertFalse(sellerValidator.isPhoneNumberInvalid(phoneNumber));
+    }
+
+    @Test
+    void testIsEmailValid() {
+        String validEmail = "Bob@bob.bob";
+
+        assertFalse(sellerValidator.isEmailInvalid(validEmail));
+    }
+
+    @Test
+    void testIsEmailMissingAt() {
+        String Email = "Bobbob.bob";
+
+        assertTrue(sellerValidator.isEmailInvalid(Email));
+    }
+
+    @Test
+    void testIsEmailMissingDot() {
+        String Email = "Bob@bobbob";
+
+        assertTrue(sellerValidator.isEmailInvalid(Email));
+    }
+
+    @Test
+    void testIsEmailMissingIdentifier() {
+        String Email = "@bob.bob";
+
+        assertTrue(sellerValidator.isEmailInvalid(Email));
+    }
+
+    @Test
+    void testIsEmailMissingExtension() {
+        String Email = "bob@bob.";
+
+        assertTrue(sellerValidator.isEmailInvalid(Email));
+    }
+
+    @Test
+    void testIsEmailMissingService() {
+        String Email = "Bob@.bob";
+
+        assertTrue(sellerValidator.isEmailInvalid(Email));
+    }
+
+    @Test
+    void testisBirthdateFormatInvalid() {
+        String invalidDate = "2001-13-20";
+
+        assertTrue(sellerValidator.isBirthdateFormatInvalid(invalidDate));
+    }
+
+    @Test
+    void testIs18orMoreCheckWithOffset() {
+        OffsetDateTime fixedDateTime = OffsetDateTime.of(2018, 1, 2, 2, 0, 0, 0, ZoneOffset.ofHours(3));
+        String invalidBirthDate1 = "2000-01-03";
+        String invalidBirthDate2 = "2000-01-02";
+        String validDate1 = "2000-01-01";
+
+        doReturn(fixedDateTime).when(sellerValidatorSpy).currentTime();
+
+        assertFalse(sellerValidatorSpy.isBirthdate18orMore(invalidBirthDate1));
+        assertFalse(sellerValidatorSpy.isBirthdate18orMore(invalidBirthDate2));
+        assertTrue(sellerValidatorSpy.isBirthdate18orMore(validDate1));
+    }
+}
