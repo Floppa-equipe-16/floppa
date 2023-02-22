6895def0e7c7914f419152a1cc678748b342dade
Alex Prudhomme <prudhomme.alex42@gmail.com>
2023-02-22 12:14:29 -0500

    style: spotless format

--- src/test/java/ulaval/glo2003/api/offer/OfferRequestTest.java
+++ src/test/java/ulaval/glo2003/api/offer/OfferRequestTest.java
@@ -1,14 +1,15 @@
 package ulaval.glo2003.api.offer;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ulaval.glo2003.domain.exceptions.MissingParamException;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 public class OfferRequestTest {
     public static final double AMOUNT = 200d;
-    public static final String MESSAGE = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum vestibulum turpis sed egestas.";
+    public static final String MESSAGE =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum vestibulum turpis sed egestas.";
     private final OfferRequest offerRequest = new OfferRequest();
 
     @BeforeEach
@@ -16,12 +17,14 @@ public class OfferRequestTest {
         offerRequest.amount = AMOUNT;
         offerRequest.message = MESSAGE;
     }
+
     @Test
     public void validateMethodThrowsWhenNullAmount() {
         offerRequest.amount = null;
 
         assertThrows(MissingParamException.class, offerRequest::validate);
     }
+
     @Test
     public void validateMethodThrowsWhenNullMessage() {
         offerRequest.message = null;
