199b2591d9ecb1c6993fae5841a7b4c9ebb9161a
AnthonySirois <AnthonySirois999@gmail.com>
2023-02-27 08:51:08 -0500

    test: added a simple test for the repo manager factory

---
+++ src/test/java/ulaval/glo2003/service/RepositoryManagerFactoryTest.java
@@ -0,0 +1,18 @@
+package ulaval.glo2003.service;
+
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+class RepositoryManagerFactoryTest {
+
+
+    @Test
+    public void canCreate() {
+        RepositoryManagerFactory factory = new RepositoryManagerFactory();
+
+        RepositoryManager manager = factory.create();
+
+        assertThat(manager).isNotNull();
+    }
+}
