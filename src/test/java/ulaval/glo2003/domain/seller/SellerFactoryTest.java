d989636f527dbc7fa59ce0db31c16d53b7b69e7c
AnthonySirois <AnthonySirois999@gmail.com>
2023-03-04 08:08:59 -0500

    test: add SellerFactoryTest

---
+++ src/test/java/ulaval/glo2003/domain/seller/SellerFactoryTest.java
@@ -0,0 +1,38 @@
+package ulaval.glo2003.domain.seller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ulaval.glo2003.domain.exceptions.InvalidParamException;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SellerFactoryTest {
+
+    private SellerFactory factory;
+
+    //TODO: Besoin de dependency injection pour la validation?
+    @BeforeEach
+    public void setUp() {
+        factory = new SellerFactory();
+    }
+
+    @Test
+    public void canCreateValidSeller() {
+        Seller seller = factory.createSeller("Alice", "2000-01-01", "Alice@email.com", "11234567890", "This");
+
+        assertThat(seller.getId()).isNotEmpty();
+        assertThat(seller.getCreatedAt()).isNotEmpty();
+        assertThat(seller.getName()).isEqualTo("Alice");
+        assertThat(seller.getBirthdate()).isEqualTo("2000-01-01");
+        assertThat(seller.getEmail()).isEqualTo("Alice@email.com");
+        assertThat(seller.getPhoneNumber()).isEqualTo("11234567890");
+        assertThat(seller.getBio()).isEqualTo("This");
+    }
+
+    @Test
+    public void createSellerThrowsWhenArgsAreEmpty() {
+        assertThrows(InvalidParamException.class, () -> factory.createSeller("", "", "", "", ""));
+    }
+
+}
