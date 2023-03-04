043a34f296c7e81d07c02b214d27f77997879cee
AnthonySirois <AnthonySirois999@gmail.com>
2023-03-04 09:11:22 -0500

    test: add constructor test for productFilter
    It's useful only because of the if null then default

---
+++ src/test/java/ulaval/glo2003/domain/product/ProductFilterTest.java
@@ -0,0 +1,32 @@
+package ulaval.glo2003.domain.product;
+
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductFilterTest {
+
+    @Test
+    public void canCreateProductFilter() {
+        ProductFilter productFilter = new ProductFilter("SELLER", "TITLE", ProductCategory.other.toString(), 10d, 100d);
+
+        assertThat(productFilter.getSellerId()).isEqualTo("SELLER");
+        assertThat(productFilter.getTitle()).isEqualTo("TITLE");
+        assertThat(productFilter.getCategory()).isEqualTo(ProductCategory.other.toString());
+        assertThat(productFilter.getMinPrice()).isEqualTo(10d);
+        assertThat(productFilter.getMaxPrice()).isEqualTo(100d);
+    }
+
+    @Test
+    public void createProductFilterHasDefaultValuesWhenArgsAreNull() {
+        ProductFilter productFilter = new ProductFilter(null, null, null, null, null);
+
+        assertThat(productFilter.getSellerId()).isNotNull();
+        assertThat(productFilter.getTitle()).isNotNull();
+        assertThat(productFilter.getCategory()).isNotNull();
+        assertThat(productFilter.getMinPrice()).isNotNull();
+        assertThat(productFilter.getMaxPrice()).isNotNull();
+    }
+
+}
