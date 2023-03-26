7de427b44d5e152610fe1299ff841d0a52cf2a62
Jérôme <jerome.levesque.14@ulaval.ca>
2023-03-26 18:24:37 -0400

    feat : added repository implementation using MongoDB and Morphia to Selling service factory

--- src/main/java/ulaval/glo2003/api/LostResource.java
+++ src/main/java/ulaval/glo2003/api/LostResource.java
@@ -2,12 +2,13 @@ package ulaval.glo2003.api;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
 
 @Path("/{else}")
 public class LostResource {
     @GET
-    public Response lost() {
+    public Response lost(@PathParam("else") String ignored) {
         return Response.status(404).build();
     }
 }
--- src/main/java/ulaval/glo2003/service/SellingServiceFactory.java
+++ src/main/java/ulaval/glo2003/service/SellingServiceFactory.java
@@ -1,21 +1,42 @@
 package ulaval.glo2003.service;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import dev.morphia.Datastore;
+import dev.morphia.Morphia;
+import java.util.concurrent.TimeUnit;
 import ulaval.glo2003.domain.offer.IOfferRepository;
-import ulaval.glo2003.domain.offer.InMemoryOfferRepository;
+import ulaval.glo2003.domain.offer.MongoOfferRepository;
 import ulaval.glo2003.domain.offer.OfferFactory;
 import ulaval.glo2003.domain.product.IProductRepository;
-import ulaval.glo2003.domain.product.InMemoryProductRepository;
+import ulaval.glo2003.domain.product.MongoProductRepository;
 import ulaval.glo2003.domain.product.ProductFactory;
 import ulaval.glo2003.domain.seller.ISellerRepository;
-import ulaval.glo2003.domain.seller.InMemorySellerRepository;
+import ulaval.glo2003.domain.seller.MongoSellerRepository;
 import ulaval.glo2003.domain.seller.SellerFactory;
 
 public class SellingServiceFactory {
 
+    private final String FLOPPA_MONGO_CLUSTER_URL =
+            "mongodb+srv://root:ulaval@floppa-staging.sasi1f4.mongodb.net/?retryWrites=true&w=majority";
+    private final String FLOPPA_MONGO_DATABASE = "floppa-staging";
+
     public SellingService create() {
-        ISellerRepository sellerRepository = new InMemorySellerRepository();
-        IProductRepository productRepository = new InMemoryProductRepository();
-        IOfferRepository offerRepository = new InMemoryOfferRepository();
+        MongoClient client = MongoClients.create(MongoClientSettings.builder()
+                .applyToClusterSettings(builder -> builder.serverSelectionTimeout(5000, TimeUnit.MILLISECONDS))
+                .applyToConnectionPoolSettings(builder -> builder.maxConnectionIdleTime(5000, TimeUnit.MILLISECONDS))
+                .applyConnectionString(new ConnectionString(FLOPPA_MONGO_CLUSTER_URL))
+                .build());
+        healthChek(client);
+        Datastore datastore = Morphia.createDatastore(client, FLOPPA_MONGO_DATABASE);
+        datastore.getMapper().mapPackage("ulaval.glo2003");
+        datastore.ensureIndexes();
+
+        ISellerRepository sellerRepository = new MongoSellerRepository(datastore);
+        IProductRepository productRepository = new MongoProductRepository(datastore);
+        IOfferRepository offerRepository = new MongoOfferRepository(datastore);
 
         SellerFactory sellerFactory = new SellerFactory();
         ProductFactory productFactory = new ProductFactory();
@@ -27,5 +48,9 @@ public class SellingServiceFactory {
 
         return new SellingService(
                 sellerRepository, productRepository, offerRepository, sellerMapper, productMapper, offerMapper);
+    }
+
+    private void healthChek(MongoClient client) {
+        client.listDatabaseNames().first();
     }
 }
