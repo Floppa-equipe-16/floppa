c9497ea98576d494e810be9571c1b6167d11bad7
Jeremi Girard <jeremigirard@hotmail.com>
2023-04-04 14:06:51 -0400

    Feat : Add equals function

--- src/main/java/ulaval/glo2003/api/offer/OfferCollectionResponse.java
+++ src/main/java/ulaval/glo2003/api/offer/OfferCollectionResponse.java
@@ -2,6 +2,7 @@ package ulaval.glo2003.api.offer;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
+import java.util.Objects;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class OfferCollectionResponse {
@@ -12,4 +13,26 @@ public class OfferCollectionResponse {
     public Double maxAmount;
 
     public List<OfferResponse> items;
+
+    public OfferCollectionResponse() {}
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (o instanceof OfferCollectionResponse) {
+            OfferCollectionResponse response = ((OfferCollectionResponse) o);
+            return isEqualsTo(response);
+        } else return false;
+    }
+
+    private boolean isEqualsTo(OfferCollectionResponse response) {
+        return Objects.equals(count, response.count)
+                && Objects.equals(avgAmount, response.avgAmount)
+                && Objects.equals(minAmount, response.minAmount)
+                && Objects.equals(maxAmount, response.maxAmount)
+                && Objects.equals(items, response.items);
+    }
 }
