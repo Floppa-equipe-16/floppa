0cd94896c30c4ca51067f72980721c1dc41ebbfe
Jeremi Girard <jeremigirard@hotmail.com>
2023-04-04 14:03:37 -0400

    Refactor : moved file to e2e test

--- src/test/java/ulaval/glo2003/e2e/HealthResourceTest.java
+++ src/test/java/ulaval/glo2003/e2e/HealthResourceTest.java
@@ -1,12 +1,10 @@
-package ulaval.glo2003.api;
+package ulaval.glo2003.e2e;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import jakarta.ws.rs.core.HttpHeaders;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Test;
-import ulaval.glo2003.e2e.ApiTestUtils;
+import ulaval.glo2003.api.HealthResponse;
 
 public class HealthResourceTest extends ApiTestUtils {
 
@@ -16,7 +14,7 @@ public class HealthResourceTest extends ApiTestUtils {
         HealthResponse healthResponse = response.readEntity(HealthResponse.class);
 
         assertThat(response.getStatus()).isEqualTo(200);
-        assertThat(response.getHeaderString(HttpHeaders.CONTENT_TYPE)).isEqualTo(MediaType.APPLICATION_JSON);
+        assertMediaTypeIsJson(response);
 
         assertThat(healthResponse.api).isTrue();
         assertThat(healthResponse.db).isTrue();
