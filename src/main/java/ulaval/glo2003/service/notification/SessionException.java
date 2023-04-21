3fda1b49689192cf3490f6308832c5d9c72f7988
Jeremi Girard <jeremigirard@hotmail.com>
2023-04-21 13:23:44 -0400

    Refactor : Change SessionException error message

--- src/main/java/ulaval/glo2003/service/notification/SessionException.java
+++ src/main/java/ulaval/glo2003/service/notification/SessionException.java
@@ -3,6 +3,6 @@ package ulaval.glo2003.service.notification;
 public class SessionException extends Exception {
 
     public SessionException() {
-        super("Session fail to send message");
+        super("Session fail to connect");
     }
 }
